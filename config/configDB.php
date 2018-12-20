95e3e3194fd29dfb16edeeea59d8770bab11e674
Alexsandro <alexsandro.ruiz@ikasle.egibide.org>
2018-12-20 10:17:54 +0100

    a

--- config/configDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("DB_DRIVER", "mysql");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_DATABASE", "empleados");
-
-?>
--- config/global.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("CONTROLADOR_DEFECTO", "Empleados");
-define("ACCION_DEFECTO", "index");
-?>
